ec5416c66299a46d41ccaa57181821cb4584b3c3
Charlie Sahlman <csahlman@gmail.com>
2013-08-17 21:11:16 -0600

    rails 4, bootstrap, backbone initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4bootstrap3backbone::Application.config.secret_key_base = 'e2e46edc3b41df2a2535e107fb3bbcaf98cdb572979e5d88a0596217d23b8396ee059555360dbea524a5a81cbf42e077c42fd047b2d4b62501a86e05bad714b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4bootstrap3backbone::Application.config.session_store :cookie_store, key: '_rails4bootstrap3backbone_session'
